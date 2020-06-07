2b489b3cc6948956fef5b2f64107521ade9efffc
Pedro Evangelista <pedroevangelista@outlook.com>
2020-06-07 20:49:29 -0300

    User profile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -14,6 +15,7 @@ const Routes: React.FC = () => (
     <Route path="/forgot-password" exact component={ForgotPassword} />
     <Route path="/reset-password" exact component={ResetPassword} />
 
+    <Route path="/profile" exact component={Profile} isPrivate />
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
